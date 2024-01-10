1689d61f61615eb50401548b4c2659d859526724
Bouncer <dev@bouncer.nl>
2024-01-10 10:17:31 +0100

    Fixed error in calculating item needs for multiple recipes

--- item.js
+++ item.js
@@ -43,7 +43,7 @@ export class Item {
             return totals
         }
         for (let ing of recipe.ingredients) {
-            let subtotals = ing.item.produce(spec, rate.mul(ing.amount), ignore, totals.itemRates, this.key)
+            let subtotals = ing.item.produce(spec, rate.mul(ing.amount), ignore, totals.itemRates, this.key+"/"+parent)
             totals.combine(subtotals)
         }
         return totals
