7580fbf18e82d3a8121cb60541b2386bd4ce2965
Bouncer <dev@bouncer.nl>
2024-03-12 16:15:55 +0100

    fix recine copper

--- init.js
+++ init.js
@@ -39,7 +39,8 @@ export function init() {
     log.add('log','Update info Feb 28th: \n- Added vehicle and liberty recipes and items')
     log.add('log','Update info March 5th: \n- Added planner animations\n- Added trip details to items page\n- Fixed vehicle recipe issues\n- Fixed items page amounts\n- Fixed Gravel Pulverizing calculation\n- Fixed ignoring of old factions\n- Fixed item names of old factions')
     log.add('log','Update info March 7th: \n- Added auto update synchronisation\n- Increased update speed')
-    log.add('info','Update info March 8th: \n- Fixed auto update for beta server')
+    log.add('log','Update info March 8th: \n- Fixed auto update for beta server')
+    log.add('info','Update info March 12th: \n- Fixed Refine Copper recipe not working')
     let settings = loadSettings(window.location.hash)
     loadData(settings)
 }
